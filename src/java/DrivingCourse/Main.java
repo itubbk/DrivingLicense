6f1bb3bc4cdc42b300092499cdc87f5949d71447
cakirdo <doganckr@gmail.com>
2016-06-25 18:04:04 +0300

    DB Connection is done

--- src/java/DrivingCourse/Main.java
+++ src/java/DrivingCourse/Main.java
@@ -5,6 +5,11 @@
  */
 package DrivingCourse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  *
  * @author dogan.cakir
@@ -14,8 +19,12 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/DrivingCourse";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "Sade01pass");
+        Connection conn = DriverManager.getConnection(url, props);
     }
-    
+
 }
